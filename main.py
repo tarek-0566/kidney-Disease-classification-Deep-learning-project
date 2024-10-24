7690133221f01388e63342f22e0b573905f24228
Md. Tarekul Islam <tarek111405iut@gmail.com>
2024-10-24 13:29:34 +0200

    Data ingestion part completed

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-from src.cnnClassifier import logger
+import sys
+import os
+import joblib
 
-logger.info("Welcome to our custom log")
+# Add the src folder to the Python path
+project_dir = r'C:/Users/islam/Downloads/kidney-Disease-classification-Deep-learning-project/src'
+sys.path.append(project_dir)
+
+
+from src.cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion stage"
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
